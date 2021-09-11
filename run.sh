4db8ee34aa6ab1df93e053032b23cfc5d2dbaa35
Husni Zuhdi <hazunanafaru@gmail.com>
2021-09-11 15:36:00 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Change energy
 # for i in {50..100..1}
 mkdir results
-for i in {50..100..1}
+for i in {50..51..1}
 do
 cd /home/mohmiftakhuldwi99_mail_ugm_ac_id/brain_phits
 mkdir  "results/testing-$i"
@@ -11,4 +11,5 @@ cp TA.inp "results/testing-$i/TA.inp"
 cd "results/testing-$i"
 sed -i "s/e0		= 80/e0		= $i/" TA.inp
 echo "Start simulating $i Energy"
+sudo phits.sh TA.inp
 done
